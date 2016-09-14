496692d5235a07df4f6ca645dbc8f53a26356433
suresh753 <kasthurisuri53@gmail.com>
2016-09-14 15:19:44 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,20 +1,31 @@
+from exceptions import Exception
 lines=[]
 total=0
 fail=0
-with open ('/home/ec2-user/.jenkins/jobs/TestEnv/lastSuccessful/log','rt') as in_file:
+Req_Test_Per = 80
+with open ('log.txt','rt') as in_file:
     for line in in_file:
         lines.append(line.rstrip('\n'))
 index = 0
 for element in lines:
     substr = "Tests run:"
     index = element.find(substr, index)
-   # print(index)
     if index>0:
-        total=int(element[23:25])
-        fail=int(element[37:38])
+        total_start = element.find(':',index)
+        total_end = element.find(',',index)
+        index = total_end
+        fail_start=element.find(':',index)
+        fail_end = element.find(',',index+1)
+        total=int(element[total_start+2:total_end])
+        fail=int(element[fail_start+2:fail_end])
         break
     index=0
 print("TestCases Run :" +str(total))
 print("Failures :" +str(fail))
-percentage = (float(total-fail)/total)*100
-print("Percentage :"+str(percentage)+"%")
+Out_Per = (float(total-fail)/total)*100
+print("Percentage :"+str(Out_Per)+"%")
+
+if  Out_Per >= Req_Test_Per:
+    print ("Passed for deploying in testenv")
+else:
+    raise Exception("build failed because of Required Percentage "+str(Req_Test_Per)+"%")
